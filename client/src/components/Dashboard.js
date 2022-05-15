3ad017e0034076e01f983bb0bcb1aebfa8b8a614
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-14 23:48:24 -0700

    fixed duplicate function on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -17,44 +17,42 @@ export default function Dashboard({ code }) {
     setCurrentGuess(guess)
   }
 
-  export default function Dashboard({ code }) {
-    const { accessToken, refreshToken } = useAuth(code)
+  // placeholder function to be replaced by guess array
+  let placeholderArray = [0, 1, 2, 3, 4, 5]
 
-    // placeholder function to be replaced by guess array
-    let placeholderArray = [0, 1, 2, 3, 4, 5]
-
-    // creates the fields that holds the guesses
-    const guessDisplay = placeholderArray.map((answer, index) => {
-      return (
-        <FullWidthTextField
-          className="guess"
-          answer={answer}
-          label={index + 1}
-        />
-      )
-    })
-    const submitAnswer = () => {
-      console.log("currentGuess", currentGuess);
-      const newAnswers = [...answers]
-      newAnswers.push(currentGuess)
-      setAnswers(newAnswers)
-    }
-
-
+  // creates the fields that holds the guesses
+  const guessDisplay = placeholderArray.map((answer, index) => {
     return (
-      <div className="body">
-        <ResponsiveAppBar />
-        <div className="guess-container">
-          {guessDisplay}
-        </div>
-        <div className="player">
-          <Player accessToken={accessToken} refreshToken={refreshToken} makePostRequesttoRefresh={makePostRequesttoRefresh} />
-        </div>
-        <div className="submit-form">
-          <ComboBox className="combo-box" getGuess={getGuess} />
-          <ColorButtons submitAnswer={submitAnswer} />
-        </div>
-      </div>
-
+      <FullWidthTextField
+        className="guess"
+        answer={answer}
+        label={index + 1}
+      />
     )
+  })
+
+  const submitAnswer = () => {
+    console.log("currentGuess", currentGuess);
+    const newAnswers = [...answers]
+    newAnswers.push(currentGuess)
+    setAnswers(newAnswers)
   }
+
+
+  return (
+    <div className="body">
+      <ResponsiveAppBar />
+      <div className="guess-container">
+        {guessDisplay}
+      </div>
+      <div className="player">
+        <Player accessToken={accessToken} refreshToken={refreshToken} makePostRequesttoRefresh={makePostRequesttoRefresh} />
+      </div>
+      <div className="submit-form">
+        <ComboBox className="combo-box" getGuess={getGuess} />
+        <ColorButtons submitAnswer={submitAnswer} />
+      </div>
+    </div>
+
+  )
+}
